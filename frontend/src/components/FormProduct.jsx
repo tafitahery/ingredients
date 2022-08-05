029f0e7937389e787f7c405787804b1c539d2854
Tafita Hery <tafitahery@gmail.com>
2022-08-05 05:39:20 +0300

    create page Product

---
+++ frontend/src/components/FormProduct.jsx
@@ -0,0 +1,98 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import OptionStock from './OptionStock';
+import InputStock from './InputStock';
+
+export default function FormProduct({ products, getData }) {
+  // state
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredientSelected, setIngredientSelected] = useState('');
+  const [quantity, setQuantity] = useState(0.0);
+  const [minStock, setMinStock] = useState(10);
+
+  // comportement
+  useEffect(() => {
+    getIngredients();
+  }, []);
+
+  const getIngredients = () => {
+    axios
+      .get('http://localhost:4000/ingredients')
+      .then(({ data }) => setIngredients(data));
+  };
+
+  const getIngredient = (id) => {
+    return ingredients.find(
+      (ingredient) => ingredient.id.toString() === id.toString()
+    );
+  };
+
+  const handleSelect = (event) => {
+    setIngredientSelected(event.target.value);
+  };
+
+  const handleQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const ingredient = getIngredient(ingredientSelected);
+
+  const newQuantity = ingredient;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!ingredientSelected) {
+      alert('Vous devez remplir le champ nom');
+      return;
+    }
+
+    const data = {
+      name: ingredient.name,
+      quantity: newQuantity ? newQuantity : ingredient.quantity,
+      stockMin: parseFloat(minStock),
+    };
+
+    axios
+      .put('http://localhost:4000/ingredients/' + ingredientSelected, data)
+      .then(() => {
+        getData();
+        setIngredientSelected('');
+        setQuantity(0.0);
+        setMinStock(10);
+      });
+  };
+
+  // affichage (render)
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nom du produit</label>
+        <input type="text" id="name" placeholder="Nom du nouveau produit" />
+      </div>
+      <div>
+        <label htmlFor="ingredient">Ingredient</label>
+        <select
+          id="ingredient"
+          value={ingredientSelected}
+          onChange={handleSelect}
+        >
+          <option value=""> --- </option>
+          {ingredients.map((ingredient) => (
+            <OptionStock key={ingredient.id} ingredient={ingredient} />
+          ))}
+        </select>
+        <div>
+          <label htmlFor="quantity">Quantité</label>
+          <InputStock
+            id="quantity"
+            value={quantity}
+            onChange={handleQuantity}
+          />
+        </div>
+        <button>Ajouter</button>
+      </div>
+      <button>Valider</button>
+    </form>
+  );
+}
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,12 +1,65 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import TableIn from '../components/TableIn';
+import FormProduct from '../components/FormProduct';
+
 function Product() {
   // state
-
+  const [products, setProducts] = useState([]);
   // comportement
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get('http://localhost:4000/products')
+      .then(({ data }) => setProducts(data));
+  };
+
+  const handleDelete = (id) => {
+    if (window.confirm('Voulez vous supprimer ce produit ?')) {
+      axios
+        .delete('http://localhost:4000/products/' + id)
+        .then(() => getData());
+    }
+  };
+
+  const handleEdit = (id) => {};
 
   // affichage (render)
   return (
-    <div>
-      <h1>Nouveau produit</h1>
+    <div className="page">
+      <h1>Produit</h1>
+      <div className="container">
+        <div className="item">
+          <h2>Liste des produit</h2>
+          <table className="tableIn">
+            <thead>
+              <tr>
+                <th>Nom produit</th>
+                <th>Liste des ingredients</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((ingredient) => (
+                <TableIn
+                  key={ingredient.id}
+                  ingredient={ingredient}
+                  handleEdit={handleEdit}
+                  handleDelete={handleDelete}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="item">
+          <h2>Gestion du stock</h2>
+          <FormProduct products={products} getData={getData} />
+        </div>
+      </div>
     </div>
   );
 }

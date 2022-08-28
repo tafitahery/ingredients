3d30422671ffe74233c87f23005d646536dd43e5
Tafita Hery <tafitahery@gmail.com>
2022-08-28 11:26:00 +0300

    Backend: Add CRUD User

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -27,3 +27,21 @@ exports.getAllUser = (req, res, next) => {
     .then((users) => res.status(200).json(users))
     .catch((error) => res.status(400).json({ error }));
 };
+
+exports.getOneUser = (req, res, next) => {
+  User.findOne({ _id: req.params.id })
+    .then((user) => res.status(200).json(user))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.modifyUser = (req, res, next) => {
+  User.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Utilisateur modifié !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.deleteUser = (req, res, next) => {
+  User.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Utilisateur supprimé !' }))
+    .catch((error) => res.status(400).json({ error }));
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,5 +7,8 @@ const router = express.Router();
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.get('/', userCtrl.getAllUser);
+router.get('/:id', userCtrl.getOneUser);
+router.put('/:id', userCtrl.modifyUser);
+router.delete('/:id', userCtrl.deleteUser);
 
 module.exports = router;
